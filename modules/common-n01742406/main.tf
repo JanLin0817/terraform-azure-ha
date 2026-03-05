8498a6ec337ebdca12c6fc370fa7d818ec58ec33
JanLin0817 <e8o1e8o1s7@gmail.com>
2026-03-05 00:23:11 -0500

    feat: add log, valut and sa

---
+++ modules/common-n01742406/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "n01742406-law"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  tags                = var.common_tags
+}
+
+resource "azurerm_recovery_services_vault" "rsv" {
+  name                = "n01742406-rsv"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
+  tags                = var.common_tags
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "n01742406commonsa"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags                     = var.common_tags
+}
--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "virtual_network_name" {
 output "subnet_name" {
   value = module.network.subnet_name
 }
+
+output "log_analytics_workspace_name" {
+  value = module.common.log_analytics_workspace_name
+}
+
+output "recovery_services_vault_name" {
+  value = module.common.recovery_services_vault_name
+}
+
+output "storage_account_name" {
+  value = module.common.storage_account_name
+}
